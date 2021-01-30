b9610a15acb4da5119e0fadec0e789d9ff0a2399
Bekir AYNIHAN <bekir.aynihan@erciyes.com>
2021-01-31 00:18:17 +0300

    Add project files.

---
+++ Entitties/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Entitties.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entitties.Concrete
+{
+    //Çıplak class kalmasın
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
